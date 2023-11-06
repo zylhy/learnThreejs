390e75b8ef470438e5b51d824ff7689a38aaed71
zylhy <1097267712@qq.com>
2023-11-06 19:48:48 +0800

    引入自动导入依赖

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
- server:{
-    host:'0.0.0.0',
+  plugins: [vue(), AutoImport({
+    imports: ['vue', 'vue-router', {
+      '@/utils/index.js': [
+        'requireImg'
+      ]
+    }
+    ],
+    dts: "src/auto-import.d.ts",
+  }), Components({
+    // 引入组件的,包括自定义组件
+    // 存放的位置
+    dts: "src/components.d.ts",
+  })],
+resolve:{
+  alias:{
+    '@':resolve(__dirname,'./src')
+    
+  }
+},
+  server: {
+    host: '0.0.0.0',
   }
 })
